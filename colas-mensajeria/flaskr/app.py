1c61fef5568e19ce2c31709c2d42fb74c23b1c33
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-09-06 23:36:24 -0500

    Ajustes en colas de mensajeria

--- colas-mensajeria/flaskr/app.py
+++ colas-mensajeria/flaskr/app.py
@@ -1,6 +1,9 @@
 from flaskr import create_app
 from flask_restful import Api
 from .vistas import VistaColaPublicar
+import logging
+
+logging.basicConfig(filename='../log.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s : %(message)s')
 
 app = create_app('default')
 app_context = app.app_context()
--- colas-mensajeria/flaskr/colas/colas.py
+++ colas-mensajeria/flaskr/colas/colas.py
@@ -4,6 +4,6 @@ celery = Celery('tasks', broker='redis://localhost:6379/0')
 
 @celery.task(name="mensaje_microservicio")
 def mensaje_microservicio(mensaje):
-    respuesta = "Enviado a redis: "+mensaje
-    print(respuesta)
+    respuesta = "Enviado a redis: "+mensaje   
+    logger.info(f'Enviado a redis') 
     return respuesta
