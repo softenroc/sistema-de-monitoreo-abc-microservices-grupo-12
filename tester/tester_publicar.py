b5998deba814386763711b4fe36d144ebcc2a625
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-09-10 09:58:11 -0500

    adicion de sleep de 1 seg

--- tester/tester_publicar.py
+++ tester/tester_publicar.py
@@ -1,19 +1,25 @@
 import requests
 import threading
+import time
 
 def publicar():
     endpoint_to_test =  'publicar'
     url = 'http://127.0.0.1:5000/'+endpoint_to_test
-    messages = 10
+    messages = 50
     count = 0
     condition = True
 
     while condition == True:
-        print(f' envío # ->'+threading.current_thread().name+str(count))
+        localtime = time.localtime()
+        result = time.strftime("%I:%M:%S %p", localtime)        
+        print(f' envío # '+result+' : '+threading.current_thread().name+str(count))
         data = '{    "tipo" : "boton_panico",    "mensaje" : "Alerta # '+threading.current_thread().name+str(count)+'! Activacion de boton de panico",    "cliente" : "'+threading.current_thread().name+str(count)+'"}'
         response = requests.post(url, data=data,headers={"Content-Type": "application/json"})
-        print(f'rta envío # -> '+str(response.status_code))
+        localtime = time.localtime()
+        result = time.strftime("%I:%M:%S %p", localtime)        
+        print(f'rta envío # '+result+' : '+str(response.status_code))
         count = count+1
+        time.sleep(1)
         if(count>=messages):
             condition = False 
 
